c5fa5acb356366b7f82e8750edea95d2d361aa41
Arash Rouhani <miffoljud@gmail.com>
2012-09-02 17:57:53 -0400

    Simplified code and implemented fig-lookup

--- src/image_comparing.py
+++ src/image_comparing.py
@@ -1,3 +1,4 @@
+import operator
 from copy import deepcopy
 from itertools import *
 
@@ -12,32 +13,30 @@ def compare_images(img1, img2):
     insert_dead(img1, len(img1))
   while(len(img2) < len(img1)):
     insert_dead(img2, len(img2))
-  return imap(aux_compare_image, repeat(img1), repeat(img2), permutations(range(n)))
+  return imap(aux_compare_image, repeat(img1), permutations(img2))
 
 def insert_dead(img, id_helper):
-  print img
   img += [("dead_" + str(id_helper), { "shape": ["val", "dead"] })]
-  print img
 
-def aux_compare_image(img1, img2, ixs):
+def aux_compare_image(img1, img2):
   """
-  Compare two same-sized images, knowing the subfigure associations
-
-  @param ixs: The asociations
+  Compare two same-sized images, knowing the subfigure associations.
+ 
+  Knowing means that we assume the 1-to-1, 2-to-2 relationship
   """
-  def ixCmps(ij):
-    i, j = ij
-    return img1[i][1], img2[j][1]
-  def resolve(pair, img_id):
-    if pair == None: return None
-    return {
-      'val': pair[1],
-      'fig': "fig-" + str(pair[1]) # TODO: implement fig lookup
-    }[pair[0]]
   return [
     {
       prop:
-        (resolve(a.get(prop), 0), resolve(b.get(prop), 1))
-    for prop in set(a.keys() + b.keys())}
-  for a, b in map(ixCmps, enumerate(ixs))]
+        (resolve(img1, a, prop), resolve(img2, b, prop))
+    for prop in set(a[1].keys() + b[1].keys())}
+  for a, b in izip(img1,img2)]
 
+def resolve(img, subfig, prop):
+  got = subfig[1].get(prop)
+  if got == None:
+    return None
+  elif got[0] == 'val':
+    return got[1]
+  else:
+    img_ids = map(operator.itemgetter(0), img)
+    return "fig-" + str(img_ids.index(got[1]))
