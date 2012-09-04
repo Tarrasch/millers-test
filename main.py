3a75cd49c5ba7770cfed92b2b09e2a2a52451909
Arash Rouhani <miffoljud@gmail.com>
2012-09-04 16:53:47 -0400

    Check for fourth file and only solve when exists

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import yaml
+import os.path
 from src.parser import parse
 from src.solver import solve
 
-for x in range(1,4):
-  tree = parse("%s.yaml" % x)
-  ans = solve(tree)
-  print "Solution to problem %s is %s" % (x, ans)
+for x in range(1,5):
+  file_path = "%s.yaml" % x
+  if os.path.exists(file_path):
+    tree = parse(file_path)
+    ans = solve(tree)
+    print "Solution to problem %s is %s" % (x, ans)
