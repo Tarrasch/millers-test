979ec08d8ac75708c8143cf255cc9006cf00d647
Arash Rouhani <miffoljud@gmail.com>
2012-09-08 23:38:03 -0400

    Add main() function to fix warnings

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os.path
 from src.parser import parse
 from src.solver import solve
 
-for x in range(1,5):
-  file_path = "reps/1-%s.txt" % x
-  if os.path.exists(file_path):
-    tree = parse(file_path)
-    ans = solve(tree)
-    print "Solution to problem %s is %s" % (x, ans)
+def main():
+  for x in range(1,5):
+    file_path = "reps/1-%s.txt" % x
+    if os.path.exists(file_path):
+      tree = parse(file_path)
+      ans = solve(tree)
+      print "Solution to problem %s is %s" % (x, ans)
+
+main()
